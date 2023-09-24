8e0f597324c79aaa4de8b0bbe0d3e596d7f14e81
Surya Teja K <shanmukhateja@gmail.com>
2023-09-24 10:47:03 +0530

    feat: add portfolio theme

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.skills-carousel-inner').slick({
+        arrows: true,
+        autoplay: true,
+        centerMode: true,
+        autoplaySpeed: 1000,
+        slidesToShow: 3
+    });
+});
